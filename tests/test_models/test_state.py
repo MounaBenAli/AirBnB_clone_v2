36f09945f4d42fe90e68eb3ee51d336dedcad2ce
MounaBenAli <benalimouna1986@gmail.com>
2022-04-11 22:03:42 +0200

    adding unittests

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,7 +1,28 @@
 #!/usr/bin/python3
 """ """
+import unittest
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
+import pep8
+
+
+class MyTestCase(unittest.TestCase):
+    @unittest.skip("demonstrating skipping")
+    def test_nothing(self):
+        self.fail("shouldn't happen")
+
+
+class Test_State_Docs(unittest.TestCase):
+    """Test documentation"""
+
+    def test_doc(self):
+        self.assertIsNotNone(State.__doc__)
+
+    def test_style_check(self):
+        """Test pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/state.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
 
 class test_state(test_basemodel):
