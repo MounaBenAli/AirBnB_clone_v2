be32fb0c81713f3cc4b31fef5d7ee96400742072
MounaBenAli <benalimouna1986@gmail.com>
2022-04-11 21:35:01 +0200

    adding unittests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
-""" """
+"""unittests for amenity.py.
+Unittest classes:
+    Test_Amenity_Docs
+    Test_Amenity_All
+"""
+import unittest
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
+import pep8
+
+
+class Test_Amenity_Docs(unittest.TestCase):
+    """Test documentation"""
+
+    def test_doc(self):
+        self.assertIsNotNone(Amenity.__doc__)
+
+    def test_style_check(self):
+        """Test pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/amenity.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
 
 class test_Amenity(test_basemodel):
