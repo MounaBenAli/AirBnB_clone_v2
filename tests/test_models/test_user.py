1387afc471134e3cacfff1989cd46a5e6a5c7136
MounaBenAli <benalimouna1986@gmail.com>
2022-04-11 22:06:34 +0200

    adding unittests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 """ """
+import unittest
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
+import pep8
+
+
+class Test_User_Docs(unittest.TestCase):
+    """Test documentation"""
+
+    def test_doc(self):
+        self.assertIsNotNone(User.__doc__)
+
+    def test_style_check(self):
+        """Test pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/user.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
 
 class test_User(test_basemodel):
