eb7366bec13acdee85acf2b6e8360a152b1428b5
MounaBenAli <benalimouna1986@gmail.com>
2022-04-11 21:37:45 +0200

    updating docstring

--- models/amenity.py
+++ models/amenity.py
@@ -4,4 +4,8 @@ from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
+    """Represent an Amenity.
+    Attributes:
+        name (str): The name of the amenity.
+    """
     name = ""
