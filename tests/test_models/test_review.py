04d5f1a188019da8a99813400097e4d3e7124ce0
MounaBenAli <benalimouna1986@gmail.com>
2022-04-11 21:57:00 +0200

    adding unittests

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 """ """
+import unittest
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
+import pep8
+
+
+class Test_Review_Docs(unittest.TestCase):
+    """Test documentation"""
+
+    def test_doc(self):
+        self.assertIsNotNone(Review.__doc__)
+
+    def test_style_check(self):
+        """Test pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/review.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
 
 class test_review(test_basemodel):
