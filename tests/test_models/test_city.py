7c00f5e73485bb77b49cfa8402a23b538895ea60
MounaBenAli <benalimouna1986@gmail.com>
2022-04-11 21:47:38 +0200

    adding unittests

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -2,6 +2,21 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
+import pep8
+import unittest
+
+
+class Test_City_Docs(unittest.TestCase):
+    """Test documentation"""
+
+    def test_doc(self):
+        self.assertIsNotNone(City.__doc__)
+
+    def test_style_check(self):
+        """Test pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/city.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
 
 class test_City(test_basemodel):
