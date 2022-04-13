ff97413c9d53ee8e2a0cbc6f36c54bc6db248e51
MounaBenAli <benalimouna1986@gmail.com>
2022-04-14 00:49:02 +0200

    updating unittests

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -23,18 +23,3 @@ class Test_State_Docs(unittest.TestCase):
         style = pep8.StyleGuide(quiet=True)
         p = style.check_files(['models/state.py'])
         self.assertEqual(p.total_errors, 0, "fix pep8")
-
-
-class test_state(test_basemodel):
-    """ """
-
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "State"
-        self.value = State
-
-    def test_name3(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
