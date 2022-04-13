142d4503098c332ee557b2f1ff660d24bd29b5d2
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-04-13 18:21:12 +0100

    storage not found in import

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,7 @@
 from os import getenv
 
 storage_type = getenv("HBNB_TYPE_STORAGE")
-
+storage = None
 if storage_type == "db":
     from .engine.db_storage import DBStorage
     storage = DBStorage()
