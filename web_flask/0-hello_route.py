e70ea513826f16f6d101678fa9c97ad937aa7767
MounaBenAli <benalimouna1986@gmail.com>
2022-05-13 11:36:00 +0200

    updating documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""Starting Flask Web application """
+"""
+Starting Flask Web application
+listening on 0.0.0.0, port 5000
+Route : /: display “Hello HBNB!”
+"""
 
 from flask import Flask
 
