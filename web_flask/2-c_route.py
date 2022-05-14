a2ea59a18f34e28554fc1f6a5747346e7db77157
MounaBenAli <benalimouna1986@gmail.com>
2022-05-14 11:47:02 +0200

    adding space :D

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -28,7 +28,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def C(text):
     """"""
-    return 'C' + text.replace('_', ' ')
+    return 'C ' + text.replace('_', ' ')
 
 
 if __name__ == "__main__":
